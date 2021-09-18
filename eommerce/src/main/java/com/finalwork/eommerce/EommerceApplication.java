f13f77464a4cfb23258eb1ef2766bc9be54d16af
ziJinChampion <2932447829@qq.com>
2021-09-18 11:40:11 +0800

    构建项目目录

---
+++ eommerce/src/main/java/com/finalwork/eommerce/EommerceApplication.java
@@ -0,0 +1,13 @@
+package com.finalwork.eommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EommerceApplication.class, args);
+    }
+
+}
---
+++ eommerce/src/test/java/com/finalwork/eommerce/EommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalwork.eommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
